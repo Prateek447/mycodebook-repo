eaf64f21ed230a290956752d403114240fa7d21c
Prateek Yadav <yprateek51@gmail.com>
2022-01-10 18:39:57 +0530

    sec-commit

---
+++ src/main/java/freestyle/controller.java
@@ -0,0 +1,14 @@
+package freestyle;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class controller {
+	
+	@GetMapping("/welcome")
+	public String get() {
+	 return	 "hello guys.....";
+	}
+
+}
